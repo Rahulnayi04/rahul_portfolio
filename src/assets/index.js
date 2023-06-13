51912470f3cea2d6e8e18667cfc326d07188a0cb
rahulnayi04 <you@example.com>
2023-06-13 22:59:39 +0530

    commit

---
+++ src/assets/index.js
@@ -0,0 +1,81 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import source_code from "./Source_Code.png"
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import mui from "./tech/mui.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import kubernates from "./tech/Kubernates.png";
+import jenkins from "./tech/Jenkins.png";
+import postgresql from "./tech/PostgreSQL.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import sgl from "./company/sgl.png";
+import mbachaiwala from "./mbachaiwala.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import restapi from "./RestAPI.png";
+import pruthvi3d from "./Pruthvi_3D.png";
+
+export {
+  mbachaiwala,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mui,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  kubernates,
+  postgresql,
+  jenkins,
+  sgl,
+  pruthvi3d,
+  restapi,
+  source_code
+};
---
+++ src/constants/index.js
@@ -0,0 +1,222 @@
+import {
+  mbachaiwala,
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  nodejs,
+  redux,
+  mui,
+  tailwind,
+  mongodb,
+  git,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  kubernates,
+  postgresql,
+  jenkins,
+  sgl,
+  pruthvi3d,
+  restapi
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  
+  {
+    title: "Front end Developer",
+    icon: mobile,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "redux",
+    icon: redux,
+  },
+  {
+    name: "Mui",
+    icon: mui,
+  },
+  {
+    name: "Tailwind",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "postgreSQL",
+    icon: postgresql,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  
+];
+
+const experiences = [
+  {
+    title: "Front End Developer",
+    company_name: "Scanpoint Geomatics Limited",
+    icon: sgl,
+    iconBg: "#E6DEDD",
+    date: "July 2021 - Present",
+    points: [
+      "I played a crucial role in the development and implementation of functional tools and features for a 3D platform. This platform was designed to handle and manipulate massive geospatial data, allowing users to create, process, view, and analyze such data with ease.",
+      "One of my significant contributions was incorporating Geographic Information System (GIS) data and services into the platform. By leveraging Open Geospatial Consortium (OGC) services, I enabled geoprocessing capabilities, empowering users to perform spatial analysis and manipulation on the platform. This integration enhanced the platform's functionality and provided users with powerful tools to work with geospatial data effectively.",
+      "Additionally, I worked on API integration as part of my responsibilities. This involved integrating various APIs into the platform to enhance its capabilities and provide seamless data exchange between different systems. By integrating APIs from external services and data providers, I facilitated the platform's ability to access and retrieve geospatial data from a wide range of sources, enriching the user experience and expanding the platform's data resources.",
+      "In summary, my experience as a front-end developer involved developing and implementing tools and features for a 3D platform focused on geospatial data. I incorporated GIS data and services, integrating OGC services to enable geoprocessing functionalities. Furthermore, I worked on API integration to enhance data access and retrieval capabilities, ensuring seamless data exchange with external services and data providers."
+    ],
+  },
+  
+  {
+    title: "Internship",
+    company_name: "MBA Chai Wala",
+    icon: mbachaiwala,
+    iconBg: "#E6DEDD",
+    date: "Sept 2020 - Oct 2020",
+    points: [
+      "During my tenure at MBA Chai Wala, I had the opportunity to engage in strategic collaborations with several event management companies. As part of my role, I actively communicated and built relationships with these companies, seeking opportunities for mutual collaboration and partnership.",
+      "By establishing partnerships with event management companies, we aimed to enhance our brand credibility and expand our reach to a wider audience. Through these collaborations, we were able to offer our services and products to a larger customer base, thereby contributing to the overall growth and success of the company.",
+      "Furthermore, my efforts in building partnerships and collaborations also contributed to enhancing the overall richness of experiences provided to our customers. By leveraging the expertise and resources of event management companies, we were able to create and deliver exceptional events that catered to the diverse interests and preferences of our target audience."
+    ],
+  }
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Survey Solution",
+    description:
+      "I developed an interactive web application using that allowed multiple users to view and manage data on a map collected by a specific surveyor. The application enabled users to perform various tasks and access different forms of data visualizations, such as charts and grids, within a comprehensive dashboard.",
+    tags: [
+      {
+        name: "node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: restapi,
+    source_code_link: "https://intsrv.sgligis.com:8443/RestAPI/",
+  },
+  {
+    name: "IGiS Pruthvi 3D",
+    description:
+      "IGiS Pruthvi 3D is a Software as a Service (SaaS) platform that empowers users to effectively perform geospatial operations on raster and vector data.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nextjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: pruthvi3d,
+    source_code_link: "https://developer.sgligis.com/sgl_3d/theme/index.html",
+  }
+];
+
+export { services, technologies, experiences, testimonials, projects };
